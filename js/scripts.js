9cc403e22f45506ddd0a70806ce3d31ec10247d7
Jennifer Kinsey and Sara Schultz <jennifer@>
2017-03-22 11:51:11 -0700

    add function to identify button that is clicked by its ID

--- js/scripts.js
+++ js/scripts.js
@@ -15,13 +15,36 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
+var mathOp = function(number1, number2) {
+    if ($('.btn').is('#addBtn')) {
+      var result = add(number1, number2);
+      return result;
+      console.log(result);
+    }
+  };
+    // } else if ("#subBtn") {
+    //   result = subtract(number1, number2);
+    // } else if ("#multBtn") {
+    //   result = multiply(number1, number2);
+    // } else if ("#divBtn") {
+    //   result = divide(number1, number2);
+    // } else {
+    //   alert("TEST");
+    // }
+
+
+
+
+
 //Front-end Logic:
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+
+  $("form#calculate").click(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
+    var number1 = parseInt($("#num1").val());
+    var number2 = parseInt($("#num2").val());
+
+    result = mathOp(number1, number2);
     $("#output").text(result);
   });
 });
