c8f3e962c0eae7af66dc8a7718b9f4d77e912da1
Asia Kane and Sara Schultz <asia@>
2017-03-23 14:50:18 -0700

    update scripts.js to correct ID names.

--- js/scripts.js
+++ js/scripts.js
@@ -15,36 +15,23 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-var mathOp = function(number1, number2) {
-    if ($('.btn').is('#addBtn')) {
-      var result = add(number1, number2);
-      return result;
-      console.log(result);
-    }
-  };
-    // } else if ("#subBtn") {
-    //   result = subtract(number1, number2);
-    // } else if ("#multBtn") {
-    //   result = multiply(number1, number2);
-    // } else if ("#divBtn") {
-    //   result = divide(number1, number2);
-    // } else {
-    //   alert("TEST");
-    // }
-
-
-
-
-
 //Front-end Logic:
 $(document).ready(function() {
-
-  $("form#calculate").click(function(event) {
+  $("form#calculator").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#num1").val());
-    var number2 = parseInt($("#num2").val());
-
-    result = mathOp(number1, number2);
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else {
+      result = divide(number1, number2);
+    }
     $("#output").text(result);
   });
 });
